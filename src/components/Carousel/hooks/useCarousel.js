7c999f506cec384298cefa78d932835e904d5b54
Kelveen Raj <kelveenrajv@gmail.com>
2025-03-24 23:39:21 +0800

    fix: refactor carousel component with embla-carousel

---
+++ src/components/Carousel/hooks/useCarousel.js
@@ -0,0 +1,48 @@
+import { useCallback, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+
+const useCarousel = () => {
+  const navigate = useNavigate();
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return;
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, [emblaApi]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+    onSelect();
+    emblaApi.on("select", onSelect);
+  }, [emblaApi, onSelect]);
+
+  const scrollPrev = () => {
+    if (emblaApi) emblaApi.scrollPrev();
+  };
+
+  const scrollNext = () => {
+    if (emblaApi) emblaApi.scrollNext();
+  };
+
+  const scrollTo = (index) => {
+    if (emblaApi) emblaApi.scrollTo(index);
+  };
+
+  const handleSlideClick = (id) => {
+    navigate(`/post/${id}`);
+  };
+
+  return {
+    emblaRef,
+    selectedIndex,
+    scrollPrev,
+    scrollNext,
+    scrollTo,
+    handleSlideClick,
+  };
+};
+
+export default useCarousel;
